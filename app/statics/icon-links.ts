189e05a20709e21524d9c034f4ae2cd42beeab20
sensasi-delight <zainadam.id@gmail.com>
2025-02-23 22:39:45 +0800

    Add icons for favicon (#2)

---
+++ app/statics/icon-links.ts
@@ -0,0 +1,31 @@
+/**
+ * All <link>s for icon/favicon that will be rendered in the <head>
+ */
+const ICON_LINKS = [
+  {
+    rel: "icon",
+    type: "image/png",
+    href: "/favicon-96x96.png",
+    sizes: "96x96",
+  },
+  {
+    rel: "icon",
+    type: "image/svg+xml",
+    href: "/favicon.svg",
+  },
+  {
+    rel: "shortcut icon",
+    href: "/favicon.ico",
+  },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/apple-touch-icon.png",
+  },
+  {
+    rel: "manifest",
+    href: "/site.webmanifest",
+  },
+];
+
+export default ICON_LINKS;
