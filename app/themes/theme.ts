2fdb37c8d612eccd1580df74004f56fa5fad89ab
sensasi-delight <zainadam.id@gmail.com>
2025-02-24 03:28:10 +0800

    Improve theme colors (#6)

--- app/themes/theme.ts
+++ app/themes/theme.ts
@@ -1,10 +1,29 @@
+import { blue } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
 // Create a theme instance.
 const theme = createTheme({
   colorSchemes: {
     light: true,
-    dark: true,
+    dark: {
+      palette: {
+        primary: {
+          main: blue[500],
+        },
+        Alert: {
+          infoStandardBg: "#001b30",
+        },
+      },
+    },
+  },
+  palette: {
+    background: {
+      default: "#F1FAFF",
+      paper: "#F1FAFF",
+    },
+    primary: {
+      main: blue[800],
+    },
   },
   cssVariables: {
     colorSchemeSelector: "class",
